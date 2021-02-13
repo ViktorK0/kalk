7e5c4017095899dfb7a12b1a55ee926f144393b7
ykushyk <ykushyk@mobileiron.com>
2021-02-13 09:53:03 +0200

    Fix incorrect logic

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,56 +5,57 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-        vvedenyaZnachennya();
-        diya();
+        final double number1 =getNumber();
+        final double number2 =getNumber();
+        final String diy =vvedenyaDiy();
 
-  }
+        diya(number1, number2, diy);
+    }
 
-    public static void vvedenyaZnachennya() {
-            double chuslo1, chuslo2, rezultat;
-            String diya;
-            Scanner vidkorustuvacha = new Scanner(System.in);
-            System.out.println("Vvedit chuslo1: ");
-            chuslo1 = Double.parseDouble(vidkorustuvacha.nextLine());
-            System.out.println("Vvedit chuslo2: ");
-            chuslo2 = Double.parseDouble(vidkorustuvacha.nextLine());
-            System.out.println("Vvedit diy(+,-,*,/,): ");
-            diya = vidkorustuvacha.nextLine();
-        }
-    private static void eror() {
-            if (diya.equals("/") && chuslo2 == 0) {
+    public static double getNumber() {
+        Scanner vidkorustuvacha = new Scanner(System.in);
+        System.out.println("Vvedit chuslo: ");
+        return Double.parseDouble(vidkorustuvacha.nextLine());
+    }
+
+    public static String vvedenyaDiy() {
+        Scanner vidkorustuvacha = new Scanner(System.in);
+        System.out.println("Vvedit diy(+,-,*,/,): ");
+        return vidkorustuvacha.nextLine();
+    }
+
+    private static void eror(String diy, double number) {
+        if (diy.equals("/") && number == 0) {
             System.out.println("ne korecktno");
-            return;
-            }
+        }
     }
 
     /**
      * цей метод вираховує результат
+     *
      * @param chuslo2
      * @param chuslo1
-     * @param rezultat
      * @param diya
      * @return
      */
-        public static int diya(String chuslo2, String chuslo1, String rezultat, String diya){
-         eror();
-        if (diya.equals("+")||diya.equals("dodavannya")) {
+    public static int diya(Double chuslo1, Double chuslo2, String diya) {
+        eror(diya, chuslo2);
+        double rezultat;
+        if (diya.equals("+") || diya.equals("dodavannya")) {
             rezultat = chuslo1 + chuslo2;
+        } else if (diya.equals("-") || diya.equals("vidnimannya")) {
+            rezultat = chuslo1 - chuslo2;
+        } else if (diya.equals("*") || diya.equals("mnogennya")) {
+            rezultat = chuslo1 * chuslo2;
+        } else if (diya.equals("/") || diya.equals("dilennya")) {
+            rezultat = chuslo1 / chuslo2;
+        } else {
+            rezultat = 0D;
         }
-            else if (diya.equals("-")||diya.equals("vidnimannya")){
-                rezultat=chuslo1-chuslo2;
-            }
-                else if (diya.equals("*")||diya.equals("mnogennya")) {
-                    rezultat = chuslo1 * chuslo2;
-                }
-                    else if (diya.equals("/")||diya.equals("dilennya")){
-                        rezultat=chuslo1/chuslo2;
-                    }
-                        else{ rezultat=0;
-                        }
         System.out.println("Vash rezultat : " + rezultat);
-                        }
-                    }
+        return (int) Math.round(rezultat);
+    }
+}
 
 
 
